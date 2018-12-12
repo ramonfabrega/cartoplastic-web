3aa7cebee1a505ceb2dd6acd4d9d77d278012c05
Ramon Fabrega <ramonfabrega0@gmail.com>
2018-12-12 09:58:36 -0600

    express rerouting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -35,6 +36,14 @@ app.post('/contact', (req, res) => {
 
   return res.status(200).json({ msg: 'Message sent' });
 });
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
