5e76118dc987216841d471e9213b6486b0366e22
Ramon Fabrega <ramonfabrega0@gmail.com>
2018-12-12 20:27:45 -0600

    anonymous functional component refactor && papeleria route

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 
@@ -9,7 +9,9 @@ import Header from './components/layout/Header';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
+// Product Pages
 import Producto from './components/products/Producto';
+import Papeleria from './components/products/Papeleria';
 
 // Info Pages
 import Conozcanos from './components/info/Conozcanos';
@@ -17,48 +19,39 @@ import FAQ from './components/info/FAQ';
 import Pedidos from './components/info/Pedidos';
 import Contactenos from './components/info/Contactenos';
 
+// CSS
 import './App.css';
 
+// Data Sources
 import productosData from './components/products/data/productosData';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      location: null
-    };
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <Router>
-          <div className='App'>
-            <Header />
-            <Navbar />
-            <Route exact path='/' component={Home} />
-            {productosData.map(producto => {
-              return (
-                <Route
-                  exact
-                  path={producto.route}
-                  render={props => <Producto {...props} data={producto} />}
-                  key={producto.route}
-                />
-              );
-            })}
-            <Route exact path='/conozcanos' component={Conozcanos} />
-            <Route exact path='/faq' component={FAQ} />
-            <Route exact path='/pedidos' component={Pedidos} />
-            <Route exact path='/contactenos' component={Contactenos} />
-            <Footer />
-          </div>
-        </Router>
-      </React.Fragment>
-    );
-  }
-}
-
-export default App;
+export default () => {
+  return (
+    <Fragment>
+      <CssBaseline />
+      <Router>
+        <div className='App'>
+          <Header />
+          <Navbar />
+          <Route exact path='/' component={Home} />
+          {productosData.map(producto => {
+            return (
+              <Route
+                exact
+                path={producto.route}
+                render={props => <Producto {...props} data={producto} />}
+                key={producto.route}
+              />
+            );
+          })}
+          <Route exact path='/productos/papeleria' component={Papeleria} />
+          <Route exact path='/conozcanos' component={Conozcanos} />
+          <Route exact path='/faq' component={FAQ} />
+          <Route exact path='/pedidos' component={Pedidos} />
+          <Route exact path='/contactenos' component={Contactenos} />
+          <Footer />
+        </div>
+      </Router>
+    </Fragment>
+  );
+};
