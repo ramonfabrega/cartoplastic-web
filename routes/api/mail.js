8ea75f56855b5e807ea90f4955055a32ebf92f10
Ramon Fabrega <ramonfabrega0@gmail.com>
2021-04-19 20:55:16 -0500

    change email

--- routes/api/mail.js
+++ routes/api/mail.js
@@ -1,33 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   secure: false,
   port: 25,
   auth: {
-    user: 'cartoplasticweb@gmail.com',
-    pass: require('../../config/keys').nodemailer
+    user: "cartoplasticweb@gmail.com",
+    pass: require("../../config/keys").nodemailer,
   },
   tls: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
-router.post('/contact', (req, res) => {
+router.post("/contact", (req, res) => {
   let helperOptions = {
     from: '"Cartoplastic Web" <cartoplasticweb@gmail.com>',
-    to: 'cartoplasticweb@gmail.com',
-    subject: 'Nuevo Mensaje en el Website',
-    text: req.body.message
+    to: "ventas@cartoplastic.com",
+    subject: "Nuevo Mensaje en el Website",
+    text: req.body.message,
   };
   transporter.sendMail(helperOptions, (error, info) => {
     if (error) console.log(error);
-    console.log('Mail sent');
+    console.log("Mail sent");
   });
 
-  return res.status(200).json({ msg: 'Mail sent' });
+  return res.status(200).json({ msg: "Mail sent" });
 });
 
 module.exports = router;
